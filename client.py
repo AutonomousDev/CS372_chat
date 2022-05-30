92428bb1d02aec72677fa7abdae3e8f773797353
Cameron Bowers <cameronbowers0@gmail.com>
2022-05-30 13:54:47 -0700

    Final house keeping

--- client.py
+++ client.py
@@ -1,25 +1,38 @@
-def client():
-    import socket
+# Name: Cameron Bowers
+# OSU Email: bowercam@oregonstate.edu
+# Course: CS372 - Intro To Networking
+# Assignment: RDT
+# Due Date: 6/04/2022
+# Description: This program is a simple chat program
+# Cited sources: https://docs.python.org/3/howto/sockets.html
 
+import socket
+
+
+def client():
+    """The client half of the chat"""
     HOST = "127.0.0.1"  # The server's hostname or IP address
     PORT = 65432  # The port used by the server
-    # username = input("Username? ")
+
+    # Connect to server
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
 
         while True:
-            # prompt = username + ": "
-            # data = input(prompt)
-            # data = username + ": " + data
+            # send reply
             data = input()
             data = bytes(data, 'utf-8')
             s.sendall(data)
+
+            # Receive reply
             data = s.recv(1024)
             data = data.decode("utf-8")
+
+            # Detect disconnect
             if data == "/q":
                 data = bytes(data, 'utf-8')
-                s.sendall(data)
-                s.close()
+                s.sendall(data)     # Disconnect the client
+                s.close()           # Disconnect the self
                 return print("Disconnected")
             print(data)
 
--- server.py
+++ server.py
@@ -1,11 +1,19 @@
-# echo-server.py
+# Name: Cameron Bowers
+# OSU Email: bowercam@oregonstate.edu
+# Course: CS372 - Intro To Networking
+# Assignment: RDT
+# Due Date: 6/04/2022
+# Description: This program is a simple chat program
+# Cited sources: https://docs.python.org/3/howto/sockets.html
+
 import socket
 
 def server():
+    """The server host this connection with the client"""
     HOST = "127.0.0.1"  # Standard loopback interface address (localhost). Make "" to accept all connections.
     PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
-    # username = input("Username? ")
 
+    # Initialize the socket
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((HOST, PORT))
@@ -14,20 +22,19 @@ def server():
         with conn:
             print(f"Connected by {addr}")
             while True:
-                # decode and print
+                # Receive reply
                 data = conn.recv(1024)
                 data = data.decode("utf-8")
+
+                # Detect disconnect
                 if data == "/q":
                     data = bytes(data, 'utf-8')
-                    conn.sendall(data)
-                    conn.close()
+                    conn.sendall(data)  # Disconnect the client
+                    conn.close()        # Disconnect the self
                     return print("Disconnected")
                 print(data)
 
-                # Reply
-                # prompt = username + ": "
-                # data = input(prompt)
-                # data = username + ": " + data
+                # Send Reply
                 data = input()
                 data = bytes(data, 'utf-8')
                 conn.sendall(data)
