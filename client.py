8057468363ed0f07d08b72212eecaaa5b67c781b
Cameron Bowers <cameronbowers0@gmail.com>
2022-05-30 13:38:42 -0700

    Added disconnect

--- client.py
+++ client.py
@@ -3,18 +3,24 @@ def client():
 
     HOST = "127.0.0.1"  # The server's hostname or IP address
     PORT = 65432  # The port used by the server
-    username = input("Username? ")
+    # username = input("Username? ")
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
 
         while True:
-            prompt = username + ": "
-            data = input(prompt)
-            data = username + ": " + data
+            # prompt = username + ": "
+            # data = input(prompt)
+            # data = username + ": " + data
+            data = input()
             data = bytes(data, 'utf-8')
             s.sendall(data)
             data = s.recv(1024)
             data = data.decode("utf-8")
+            if data == "/q":
+                data = bytes(data, 'utf-8')
+                s.sendall(data)
+                s.close()
+                return print("Disconnected")
             print(data)
 
 
--- server.py
+++ server.py
@@ -4,7 +4,8 @@ import socket
 def server():
     HOST = "127.0.0.1"  # Standard loopback interface address (localhost). Make "" to accept all connections.
     PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
-    username = input("Username? ")
+    # username = input("Username? ")
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((HOST, PORT))
@@ -16,12 +17,18 @@ def server():
                 # decode and print
                 data = conn.recv(1024)
                 data = data.decode("utf-8")
+                if data == "/q":
+                    data = bytes(data, 'utf-8')
+                    conn.sendall(data)
+                    conn.close()
+                    return print("Disconnected")
                 print(data)
 
                 # Reply
-                prompt = username + ": "
-                data = input(prompt)
-                data = username + ": " + data
+                # prompt = username + ": "
+                # data = input(prompt)
+                # data = username + ": " + data
+                data = input()
                 data = bytes(data, 'utf-8')
                 conn.sendall(data)
 
